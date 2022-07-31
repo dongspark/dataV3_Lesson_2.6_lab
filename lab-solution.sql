602fe26384bbe651025fb31d8748181db66d81df
Yadong Du <duyd881@163.com>
2022-07-31 15:41:21 +0200

    add new version of solution

--- lab-solution.sql
+++ lab-solution.sql
@@ -13,15 +13,15 @@ SELECT staff_id,count(rental_id) AS rental_num FROM sakila.rental
 GROUP BY staff_id;
 
 -- 4.Using the film table, find out how many films were released each year.
-SELECT release_year,count(film_id) FROM sakila.film
+SELECT release_year,count(film_id) AS film_num FROM sakila.film
 GROUP BY release_year;
 
 -- 5.Using the film table, find out for each rating how many films were there.
-SELECT rating,count(film_id) FROM sakila.film
+SELECT rating,count(film_id) AS film_num FROM sakila.film
 GROUP BY rating;
 
 -- 6.What is the mean length of the film for each rating type. Round off the average lengths to two decimal places
-SELECT rating,round(avg(length),2) FROM sakila.film
+SELECT rating,round(avg(length),2) as avg_length FROM sakila.film
 GROUP BY rating;
 
 -- 7.Which kind of movies (rating) have a mean duration of more than two hours?
